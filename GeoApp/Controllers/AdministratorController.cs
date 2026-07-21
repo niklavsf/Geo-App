9d3adf074d90f5d12e39d87e8d9c910606a51748
niklavsf <freimanisniklavs@gmail.com>
2026-07-21 09:57:30 +0300

    Add project files.

---
+++ GeoApp/Controllers/AdministratorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeoApp.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GeoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
